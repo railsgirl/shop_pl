dee1168b399f7e546aa4102036d60a1472160b83
Paulina <paulina.recko@gmail.com>
2018-07-07 12:56:17 +0200

    Create structure of main page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,9 @@
 //= require rails-ujs
 //= require activestorage
 //= require jquery
+//= require popper
 //= require bootstrap-sprockets
 //= require turbolinks
-//= require_tree .
+//= require_tree 
+
+
